74d2689a4e2262102f9f7cfc60f896605178c6d0
iwakura1ain <ernie937@gmail.com>
2020-12-17 11:26:14 +0900

    some cron scripts

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 now="Update log for $(date) \n"
 
-echo $now > /tmp/apt.log
+/bin/echo $now > /tmp/apt.log
 
-apt-get update -qq -y --force-yes &> /tmp/apt.log 
-apt-get upgrade -qq -y --force-yes &> /tmp/apt.log 
-apt autoclean -qq -y --force-yes &> /tmp/apt.log 
+/usr/bin/apt-get update -q -y --force-yes &> /tmp/apt.log 
+/usr/bin/apt-get upgrade -q -y --force-yes &> /tmp/apt.log 
+/usr/bin/apt autoclean -q -y --force-yes &> /tmp/apt.log 

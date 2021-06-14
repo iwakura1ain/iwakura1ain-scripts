828707c977c204960793f45689d5fc1a9c2e9916
iwakura1ain <ernie937@gmail.com>
2021-06-14 11:16:02 +0000

    Finally out of the army!

---
+++ mount_hdd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mount -t vfat /dev/sda1 /home/dks/HDD -o rw,uid=1000,gid=1000
--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 now="Update log for $(date) \n"
 
-/bin/echo $now > /tmp/apt.log
+/bin/echo $now > /var/log/auto_update.log
 
-/usr/bin/apt-get update -q -y --force-yes &> /tmp/apt.log 
-/usr/bin/apt-get upgrade -q -y --force-yes &> /tmp/apt.log 
-/usr/bin/apt autoclean -q -y --force-yes &> /tmp/apt.log 
+/usr/bin/apt-get update -q -y --force-yes &>> /var/log/auto_update.log 
+/usr/bin/apt-get upgrade -q -y --force-yes &>> /var/log/auto_update.log
+/usr/bin/apt autoclean -q -y --force-yes &>> /var/log/auto_update.log
